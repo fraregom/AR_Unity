3d685b951000e0a23e15c1ff339740ede5a5bcf8
Francisco Reyes <francisco.reyesgom@gmail.com>
2020-08-10 00:54:23 -0400

    Interacting with reality in AR using a Robot

--- AR Unity/Assets/Scripts/ARLessons.cs
+++ AR Unity/Assets/Scripts/ARLessons.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.InputSystem.Utilities;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+
 
 public class ARLessons : MonoBehaviour
 {
@@ -10,6 +13,8 @@ public class ARLessons : MonoBehaviour
     [SerializeField] TMPro.TMP_Text PlanesText;
     [SerializeField] ARPlaneManager PlaneManager;
     [SerializeField] ARPointCloudManager PointCloudManager;
+    [SerializeField] ARRaycastManager RaycastManager;
+    [SerializeField] Transform Robot;
 
     private int planesadded = 0;
     private int planesupdated = 0;
@@ -18,12 +23,23 @@ public class ARLessons : MonoBehaviour
     private int pointsupdated = 0;
     private int pointsremoved = 0;
 
+    private float width;
+    private float height;
+    
+    private List<ARRaycastHit> hits;
+    private ReadOnlyArray<Touch> _touches;
+    private Transform RobotInstance; 
+    private bool robotInstantiated = false;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         ARSession.stateChanged += Events;
         PlaneManager.planesChanged += Events_planes;
         PointCloudManager.pointCloudsChanged += Events_points;
+        hits = new List<ARRaycastHit>();
+        UnityEngine.InputSystem.EnhancedTouch.EnhancedTouchSupport.Enable();
     }
 
     void Events(ARSessionStateChangedEventArgs Event){
@@ -71,6 +87,46 @@ public class ARLessons : MonoBehaviour
     void Update()
     {
         PlanesText.text = "Planes added: " + planesadded + "\n Planes updated: " + planesupdated + "\n Planes removed: " + planesremoved;
-        PlanesText.text += "\n Points added: " + pointsadded + "\n Points updated: " + pointsupdated + "\n Points removed: " + pointsremoved;  
+        PlanesText.text += "\n Points added: " + pointsadded + "\n Points updated: " + pointsupdated + "\n Points removed: " + pointsremoved; 
+        
+        _touches = Touch.activeTouches;
+ 
+        if (_touches.Count == 1)
+            SetRobot(_touches[0]);
+    }
+
+    void SetRobot(Touch touch)
+    {
+        if(RaycastManager.Raycast(touch.screenPosition, hits)){
+            
+            while(hits.Count > 0){
+                ARRaycastHit hit = hits[0];
+                hits.RemoveAt(0);
+                    
+                Vector3 positionInstance = hit.pose.position;
+
+                if (!robotInstantiated)
+                {
+                    RobotInstance = Instantiate(Robot, positionInstance, Quaternion.identity);
+                    SetPhysics();
+                    robotInstantiated = true;
+                }
+                else
+                {
+                    RobotInstance.position = positionInstance;
+                }
+
+            }
+        }
+    }
+
+    void SetPhysics()
+    {
+        Rigidbody RobotRB = Robot.GetComponent<Rigidbody>();
+        if(RobotRB != null){
+            RobotRB.isKinematic = true;
+            RobotRB.velocity *= 0;
+            RobotRB.angularVelocity *= 0;
+        }
     }
 }
